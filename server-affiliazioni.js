11f0d60811e35dc4fc51cb624c418b6f962ecfb8
saam40ac <saam40academyschool@gmail.com>
2026-03-10 11:29:05 +0100

    Update server-affiliazioni.js

--- server-affiliazioni.js
+++ server-affiliazioni.js
@@ -13,7 +13,7 @@
 
 const express  = require('express');
 const router   = express.Router();
-const bcrypt   = require('bcrypt');
+const bcrypt   = require('bcryptjs');
 const jwt      = require('jsonwebtoken');
 const crypto   = require('crypto');
 const { Pool } = require('pg');
